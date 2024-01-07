20aa2fcdbf3c4cf96f5ff10aa29fa4dfa32f5447
xev <git@schore.org>
2024-01-07 01:41:48 -0500

    Before Changing How it transmits

---
+++ src/backblaze_status/bz_prepare.py
@@ -0,0 +1,164 @@
+import re
+import time
+from dataclasses import dataclass, field
+from io import TextIOWrapper
+from pathlib import Path
+
+from .backup_file import BackupFile
+from .bz_log_file_watcher import BzLogFileWatcher
+from .main_backup_status import BackupStatus
+from .qt_backup_status import QTBackupStatus
+from .to_do_files import ToDoFiles
+from .utils import MultiLogger, get_lock, return_lock
+from icecream import ic
+import select
+from datetime import datetime
+
+ic.configureOutput(
+    includeContext=True, prefix=datetime.now().strftime("%Y-%m-%d %H:%M:S")
+)
+ic.disable()
+
+
+@dataclass
+class BzPrepare(BzLogFileWatcher):
+    backup_status: BackupStatus
+    qt: QTBackupStatus | None = field(default=None)
+    backup_list: ToDoFiles | None = field(default=None, init=False)
+    BZ_LOG_DIR: str = field(
+        default="/Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile",
+        init=False,
+    )
+    file_size: int = field(default=0, init=False)
+    previous_file: str = field(default=None, init=False)
+
+    def __post_init__(self):
+        self._multi_log = MultiLogger("BzPrepare", terminal=True, qt=self.qt)
+        self._module_name = self.__class__.__name__
+        self._multi_log.log("Starting BzPrepare")
+
+        # Compile the chunk search regular expression
+        self.chunk_search_re = re.compile(r"seq([0-9a-f]+).dat")
+
+        # 1       +       00000000026c7d44        0000018c0f260c68        10485760        /Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile/onechunk_seq00000.dat
+
+    def _get_latest_logfile_name(self) -> Path:
+        """
+        Scan the log directory for any files that end in .log, and return the one with the newest modification time
+
+        :return:
+        """
+        return Path(self.BZ_LOG_DIR) / "bz_todo_for_chunks.dat"
+
+    def _tail_file(self, _file: TextIOWrapper) -> str:
+        _log_file = self._get_latest_logfile_name()
+        while True:
+            if not _log_file.exists():
+                return
+
+            if _log_file.stat().st_size < self.file_size:
+                self.file_size = 0
+                return
+
+            # TODO: Change this from a readline to a select, and then check the file each time
+            # You can do a select() on sys.stdin, and put a timeout on the select, ie:
+            #
+            # rfds, wfds, efds = select.select( [sys.stdin], [], [], 5)
+            #
+            # would give you a five second timeout. If the timeout expired, then rfds
+            # would be an empty list. If the user entered something within five
+            # seconds, then rfds will be a list containing sys.stdin.
+
+            readable, _, _ = select.select([_file], [], [], 0.5)
+            if not readable:
+                time.sleep(0.5)
+                continue
+
+            _line = _file.readline()
+            ic(_line)
+            if not _line:
+                time.sleep(1)
+                continue
+            self.file_size = _log_file.stat().st_size
+            yield _line
+
+    def read_file(self) -> None:
+        while True:
+            if not self.backup_list:
+                # Give the main program time to start up and scan the disks
+                time.sleep(10)
+                self.backup_list = self.backup_status.to_do
+            else:
+                break
+
+        _log_file = self._get_latest_logfile_name()
+        while True:
+            if not _log_file.exists():
+                id(f"{str(_log_file)} not found")
+                time.sleep(1)
+                continue
+            try:
+                pre_stat = _log_file.stat()
+                self.file_size = pre_stat.st_size
+                # ic.enable()
+                ic(f"Starting to read {_log_file}")
+                backup_file: BackupFile = self.backup_list.current_file
+                while not backup_file:
+                    time.sleep(1)
+                    backup_file: BackupFile = self.backup_list.current_file
+
+                ic(
+                    f"Previous file: {self.previous_file}, this file: {backup_file.file_name}"
+                )
+                ic.disable()
+                while str(backup_file.file_name) == self.previous_file:
+                    time.sleep(1)
+                    backup_file: BackupFile = self.backup_list.current_file
+                    ic(
+                        f"Slept and tried again - Previous file: {self.previous_file}, this file: {backup_file.file_name}"
+                    )
+
+                self.previous_file = str(backup_file.file_name)
+                # TODO: Do soemthing to determine that the current file is not the previous file, and if it
+                #  is, then wait for it to be the current?
+                with _log_file.open("r") as _log_fd:
+                    self._multi_log.log(f"Reading file {_log_file}")
+                    for _line in self._tail_file(_log_fd):
+                        self._process_line(_line)
+
+                # ic.enable()
+                ic(f"Log file ended")
+                ic.disable()
+                _log_file = self._get_latest_logfile_name()
+            except FileNotFoundError:
+                # This is expected
+                time.sleep(1)
+
+    def _process_line(self, _line: str) -> None:
+        #         # 1       +       00000000026c7d44        0000018c0f260c68        10485760        /Library/Backblaze.bzpkg/bzdata/bzbackup/bzdatacenter/bzcurrentlargefile/onechunk_seq00000.dat
+        results = self.chunk_search_re.search(_line.strip())
+        if results is not None:
+            chunk_hex = results.group(1)
+            chunk_num: int = int(chunk_hex, base=16)
+
+            # TODO: Do soemthing to determine that the current file is not the previous file, and if it
+            #  is, then wait for it to be the current?
+
+            backup_file: BackupFile = self.backup_list.current_file
+            while backup_file is None:
+                time.sleep(1)
+                backup_file: BackupFile = self.backup_list.current_file
+
+            lock_start = get_lock(backup_file.lock, "backup_file", "bz_prepare:82")
+            backup_file.chunks_prepared.add(chunk_num)
+            return_lock(backup_file.lock, "backup_file", "bz_prepare:84", lock_start)
+            # ic(f"Preparing {str(backup_file.file_name)} chunk {chunk_num}")
+            self.backup_status.qt.signals.update_prepare.emit(
+                str(backup_file.file_name), chunk_num
+            )
+            # print(
+            #     f"Set status of {backup_file.file_name} for chunk {chunk_num} to prepared"
+            # )
+            # self.qt.signals.update_chunk_preparing.emit(
+            #    f"Preparing {str(self.backup_list.current_file.file_name)} ({chunk_num:,})"
+            # )
---
+++ src/backblaze_status/bz_transmit.py
@@ -0,0 +1,143 @@
+import re
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from pathlib import Path
+import logging
+
+from .backup_file import BackupFile
+from .bz_log_file_watcher import BzLogFileWatcher
+from .main_backup_status import BackupStatus
+from .qt_backup_status import QTBackupStatus
+from .to_do_files import ToDoFiles
+from .utils import MultiLogger, get_lock, return_lock
+
+
+@dataclass
+class BzTransmit(BzLogFileWatcher):
+    """
+    The bztransmit log contains a lot of information about the backup process. I ignore most of it, but there are
+    certain key lines that give insights into what is happening in the backup
+    """
+
+    backup_status: BackupStatus
+    qt: QTBackupStatus | None = field(default=None)
+    backup_list: ToDoFiles | None = field(default=None, init=False)
+
+    _total_lines: int = field(default=0, init=False)
+    _dedups: int = field(default=0, init=False)
+    _blank_lines: int = field(default=0, init=False)
+    _bytes: int = field(default=0, init=False)
+    _batch_count: int = field(default=0, init=False)
+    _is_batch: bool = field(default=False, init=False)
+    _current_filename: Path | None = field(default=None, init=False)
+    _first_pass: bool = field(default=True, init=False)
+    BZ_LOG_DIR: str = field(
+        default="/Library/Backblaze.bzpkg/bzdata/bzlogs/bztransmit",
+        init=False,
+    )
+
+    def __post_init__(self):
+        self._multi_log = MultiLogger("BzTransmit", terminal=True, qt=self.qt)
+        self._module_name = self.__class__.__name__
+        self._multi_log.log("Starting BzTransmit")
+
+        self.go_to_end = True
+
+        # Compile the match for prepare large file search
+        self.prepare_match_re = re.compile(
+            ".*Entering PrepareBzLargeFileDirWithLargeFile.*"
+        )
+        # Compile the search for dedup
+        self.dedup_search_re = re.compile("chunk ([^ ]*) for this largefile")
+
+        # Compile the search for new to_do file
+        self.new_to_do_file_re = re.compile("Leaving MakeBzDoneFileToDataCenter")
+
+    def _get_latest_logfile_name(self) -> Path:
+        """
+        Scan the log directory for any files that end in .log, and return the one with the newest modification time
+
+        :return:
+        """
+        last_file = None
+        _dir = Path(self.BZ_LOG_DIR)
+        for _file in _dir.iterdir():
+            if _file.suffix == ".log":
+                if not last_file:
+                    last_file = _file
+                else:
+                    if _file.stat().st_mtime > last_file.stat().st_mtime:
+                        last_file = _file
+        return last_file
+
+    def _process_line(self, _line: str) -> None:
+        _filename: Path | None = None
+        _bytes: int = 0
+        _rate: str | None = None
+        _timestamp: str | None = None
+        _datetime: datetime | None = None
+
+        _line = _line.strip()
+        self._multi_log.log(_line, level=logging.DEBUG)
+
+        # When this matches it means that there a new large file being backed up
+        match_result = self.prepare_match_re.match(_line)
+        if match_result is not None:
+            _timestamp = _line[0:19]
+            # The transmit file uses UTC time
+            _datetime = (
+                datetime.strptime(_timestamp, "%Y-%m-%d %H:%M:%S")
+                .replace(tzinfo=timezone.utc)
+                .astimezone(tz=None)
+            )
+            chunk = True
+            _filename = Path(_line.split(": ")[-1].rstrip())
+
+            if not self.backup_list.exists(_filename):
+                self.backup_list.add_file(
+                    Path(_filename),
+                    is_chunk=chunk,
+                    timestamp=_datetime,
+                )
+            lock_start = get_lock(
+                self.backup_list.lock, "backup_list", "bz_transmit:134"
+            )
+            self.backup_list.current_file = self.backup_list.file_dict[str(_filename)]
+            return_lock(
+                self.backup_list.lock, "backup_list", "bz_transmit:136", lock_start
+            )
+            self.qt.signals.start_new_file.emit(str(_filename))
+
+        _dedup_search_results = self.dedup_search_re.search(_line)
+        if _dedup_search_results is not None:
+            chunk = True
+            chunk_number = int(_dedup_search_results.group(1))
+            _filename = Path(_line.split(": ")[-1].rstrip())
+            _timestamp = _line[0:19]
+            # The transmit file uses UTC time
+            _datetime = (
+                datetime.strptime(_timestamp, "%Y-%m-%d %H:%M:%S")
+                .replace(tzinfo=timezone.utc)
+                .astimezone(tz=None)
+            )
+
+            if not self.backup_list.exists(str(_filename)):
+                self.backup_list.add_file(
+                    Path(_filename),
+                    is_chunk=chunk,
+                    timestamp=_datetime,
+                )  # add_file locks the backup list itself
+
+            file_info = self.backup_list.file_dict[str(_filename)]  # type: BackupFile
+            lock_start = get_lock(file_info.lock, "file_info", "bz_transmit:167")
+            file_info.dedup_count += 1
+            file_info.chunks_deduped.add(chunk_number)
+            file_info.chunk_current = chunk_number
+            file_info.rate = "bztransmit"
+            return_lock(file_info.lock, "file_info", "bz_transmit:172", lock_start)
+
+        _new_to_do_file_search_results = self.new_to_do_file_re.search(_line)
+        if _new_to_do_file_search_results is not None:
+            # TODO: We need to re-read the to_do file
+            self._multi_log.log(f"There is a new ToDo file. Reread it ({_timestamp})")
+            pass
