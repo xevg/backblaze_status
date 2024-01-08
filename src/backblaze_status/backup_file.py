8c28bd08ab90e55dfa6b727c5cfc0825f8d962d9
xev <git@schore.org>
2024-01-07 23:13:35 -0500

    feat: added more info fields

--- src/backblaze_status/backup_file.py
+++ src/backblaze_status/backup_file.py
@@ -1,5 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from .bz_batch import BzBatch
+from _datetime import datetime
+import threading
 
 
 @dataclass
@@ -11,7 +14,20 @@ class BackupFile:
     file_name: Path
     file_size: int
     list_index: int
+
+    timestamp: datetime = field(default=datetime.now())
     completed: bool = False
     dedup_count: int = 0
-    chunk_count: int = 0
-    bytes: int = 0
+    dedup_current: bool = False
+    deduped_bytes: int = 0
+    transmitted_bytes: int = 0
+    total_bytes_processed: int = 0
+    large_file = False
+    chunks_total: int = 0
+    chunks_prepared: set = field(default_factory=set)
+    chunks_deduped: set = field(default_factory=set)
+    chunks_transmitted: set = field(default_factory=set)
+    chunk_current: int = 0
+    batch: BzBatch = None
+    rate: str = str()
+    lock: threading.Lock = field(default_factory=threading.Lock, init=False)
