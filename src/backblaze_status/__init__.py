d9edb065f31aebb5fbbe88a8192ccd2173c7524e
Xev <38051173+xevg@users.noreply.github.com>
2024-02-17 00:51:11 -0500

    feat: trying new version with extra sauce

--- src/backblaze_status/__init__.py
+++ src/backblaze_status/__init__.py
@@ -11,6 +11,7 @@ from .backup_file import BackupFile
 
 # read version from installed package
 __version__ = version("backblaze_status")
+__extra_version__ = "v0.10.1"
 
 rich.traceback.install(show_locals=False)
 
