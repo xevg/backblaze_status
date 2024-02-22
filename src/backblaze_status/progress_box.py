5f57204a10c0c501742f0d805872db93cca73c9f
Xev <38051173+xevg@users.noreply.github.com>
2024-02-22 13:38:54 -0500

    feat: add chunk information

--- src/backblaze_status/progress_box.py
+++ src/backblaze_status/progress_box.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
-
-from .utils import file_size_string
-from .configuration import Configuration
 import threading
 import time
+from datetime import datetime, timedelta
+
+from .configuration import Configuration
 from .locks import lock, Lock
 from .to_do_files import ToDoFiles
+from .utils import file_size_string
 
 gb_divisor = Configuration.gb_divisor
 tb_divisor = Configuration.tb_divisor
@@ -19,7 +19,13 @@ class ProgressBox:
 
         self._total_size: int = 0
         self._total_size_completed: int = 0
+        self._total_size_processed: int = 0
         self._total_size_transmitted: int = 0
+
+        self._total_chunks_transmitted: int = 0
+        self._total_chunks_deduped: int = 0
+        self._total_chunks_completed: int = 0
+        self._total_chunks: int = 0
 
         self._total_files: int = 0
         self._total_files_completed: float = 0.0
@@ -33,9 +39,7 @@ class ProgressBox:
 
         self._remaining_size: int = 0
         self._time_remaining: int = 0
-        self._estimated_completion_time: str = (
-            "Estimated Completion Time: Calculating ..."
-        )
+        self._estimated_completion_time: str = "Calculating ..."
 
         self._progress_string = "No progress yet"
 
@@ -43,11 +47,14 @@ class ProgressBox:
 
         self._files_percentage = 0
         self._size_percentage = 0
+        self._chunks_percentage = 0
+
         self.last_calculated: datetime = datetime.now()
 
         timer_thread = threading.Thread(
             target=self.timer, name="ProgressUpdateTimer", daemon=True
         )
+        timer_thread.name = "ProgressUpdateTimer"
         timer_thread.start()
 
     def timer(self) -> None:
@@ -69,17 +76,32 @@ class ProgressBox:
 
         self._total_size = to_do.total_size
         self._total_size_completed = to_do.completed_size
+        self._total_size_processed = to_do.processed_size  # to_do.completed_size
         self._total_size_transmitted = to_do.transmitted_size
         self._total_files = to_do.total_file_count
         self._total_files_completed = to_do.completed_file_count
 
+        # Chunk calculation
+
+        self._total_chunks_transmitted = to_do.transmitted_chunk_count
+        self._total_chunks_deduped = to_do.duplicate_chunk_count
+        self._total_chunks = to_do.total_chunk_count
+        self._total_chunks_completed = (
+            self._total_chunks_transmitted + self._total_chunks_deduped
+        )
+        if self._total_chunks == 0:
+            self._chunks_percentage = 0
+        else:
+            self._chunks_percentage = self._total_chunks_completed / self._total_chunks
+
         # to_do.completed_size returns all the completed items, this adds the
         # currently processing file as well
-        if current_file.large_file:
+        if current_file.is_large_file:
             self._total_size_completed += current_file.total_chunk_size
             self._total_size_transmitted += current_file.transmitted_chunk_size
 
         now = datetime.now()
+
         # Files Percentage
         if to_do.total_file_count == 0:
             self._files_percentage = 0
@@ -96,16 +118,13 @@ class ProgressBox:
         if seconds_difference == 0:
             self._rate = 0
         else:
-            # Use transmitted size, because otherwise dedups can skew the rate
-            self._rate = self._total_size_transmitted / seconds_difference
+            self._rate = self._total_size_completed / seconds_difference
 
         # Calculate time remaining
 
         if self._rate == 0:
             self._time_remaining = 0
-            self._estimated_completion_time = (
-                "Estimated Completion Time: Calculating ..."
-            )
+            self._estimated_completion_time = "Calculating ..."
         else:
             self._remaining_size = to_do.remaining_size
             # Was: total_size - to_do.completed_size
@@ -131,7 +150,7 @@ class ProgressBox:
 
     @property
     def total_size_completed(self) -> int:
-        return self._total_size_completed
+        return self._total_size_processed
 
     @property
     def files_percentage(self) -> float:
@@ -154,7 +173,7 @@ class ProgressBox:
 
     @property
     def total_size_completed_gb(self) -> float:
-        return self._total_size_completed / gb_divisor
+        return self._total_size_processed / gb_divisor
 
     @property
     def progress_value(self) -> int:
@@ -167,11 +186,16 @@ class ProgressBox:
     @property
     def progress_string(self) -> str:
         progress_string = (
-            f'<span style="color: yellow">{file_size_string(self._total_size_completed)} </span> /'
+            f'<span style="color: yellow">{file_size_string(self._total_size_processed)} '
+            f"</span> /"
             f' <span style="color: yellow">{file_size_string(self._total_size)} </span> '
             f' (Files: <span style="color: yellow">{self._total_files_completed:,} </span> /'
             f' <span style="color: yellow">{self._total_files:,}</span>'
-            f' [<span style="color: magenta">{self._files_percentage:.1%}</span>])'
+            f' [<span style="color: magenta">{self._files_percentage:.1%}</span>],'
+            f' Chunks: <span style="color: yellow">'
+            f" {self._total_chunks_completed:,}</span> /"
+            f' <span style="color: yellow">{self._total_chunks:,}</span>'
+            f' [<span style="color: magenta">{self._chunks_percentage:.1%}</span>])'
         )
         return progress_string
 
