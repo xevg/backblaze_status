e483b7a0783c9dbed029aa62f55980bb0854f34f
Xev <38051173+xevg@users.noreply.github.com>
2024-05-08 12:20:02 -0400

    fix: added debugging

--- src/backblaze_status/bz_prepare.py
+++ src/backblaze_status/bz_prepare.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from pathlib import Path
 from typing import Optional
+from icecream import ic
 
 from .constants import Key, MessageTypes
 from .current_state import CurrentState
@@ -57,9 +58,12 @@ class BzPrepare:
             if not log_file.exists():
                 return
 
-            if log_file.stat().st_size < self.file_size:
-                self.file_size = 0
-                return
+            new_size = log_file.stat().st_size
+            if new_size < self.file_size:
+                ic(f"Log file shrank from {self.file_size} to {new_size}")
+                self.file_size = new_size
+                time.sleep(2)
+                continue
 
             # You can do a select() on sys.stdin, and put a timeout on the select, ie:
             #
@@ -71,6 +75,7 @@ class BzPrepare:
 
             readable, _, _ = select.select([_file], [], [], 0.5)
             if not readable:
+                ic("Waiting for line to become available")
                 # If no new line is available, wait a half second then try again
                 time.sleep(0.5)
                 continue
@@ -78,6 +83,7 @@ class BzPrepare:
             # I know there is a line waiting for me, so read it
             _line = _file.readline()
             if not _line:
+                ic("No line read")
                 break
 
             # Save the size of the file, so I can compare it next time
@@ -85,39 +91,87 @@ class BzPrepare:
             self.process_line(_line)
 
     def read_file(self) -> None:
-        log_file = self.get_latest_logfile_name()
+        log_file = Path(self.BZ_LOG_DIR) / "bz_todo_for_chunks.dat"
+        previous_file = CurrentState.CurrentFile
         while True:
+            ic("Checking if log file exists")
             if not log_file.exists():
                 time.sleep(1)
                 continue
-            try:
-                # Because there is no file name in this file, pull the name of the
-                # current file that is set in bz_transmit
-                # self.current_file: str = self.backblaze_redis.current_file
-                while CurrentState.CurrentFile is None:
+
+            location = 0
+            stat = log_file.stat()
+            old_size = stat.st_size
+            old_mtime = stat.st_mtime
+
+            def file_check():
+                if not log_file.exists():
+                    return False
+                ic("Checking if CurrentFile is set")
+                if CurrentState.CurrentFile is None:
                     # If there is no current file set, then wait till there is one
                     time.sleep(1)
+                    return False
 
                 # I don't want to keep rereading the same file over and over again
-                while CurrentState.CurrentFile == self.previous_file:
+                ic("Checking if filename has changed")
+                if CurrentState.CurrentFile == self.previous_file:
                     time.sleep(1)
+                    return True
 
+                ic(f"Preparing {CurrentState.CurrentFile}")
                 self.previous_file = CurrentState.CurrentFile
 
-                with log_file.open("r") as log_fd:
-                    self.tail_file(log_fd, log_file)
-                    # Read until the tail returns rather than yields. If it
-                    # returns, then I need to check for a new file, and start again
+                file_stat = log_file.stat()
+                if file_stat.st_size == location and file_stat.st_mtime == old_mtime:
+                    ic("No change in file")
+                    time.sleep(1)
+                    return True
+                else:
+                    if file_stat.st_size < location:
+                        ic(f"File size shrank from {file_stat.st_size} to {location}")
+                        return False
+                ic("New data available")
+                time.sleep(1)
+                return True
 
-                log_file = self.get_latest_logfile_name()
-                self._multi_log.log(f"Log file ended, opening new log file {log_file}")
-                # I need to make it so that it tries reading the new file
+            try:
+                with log_file.open("r") as log_fd:
+                    while True:
+                        # self.tail_file(log_fd, log_file)
+                        # Read until the tail returns rather than yields. If it
+                        # returns, then I need to check for a new file, and start again
+
+                        if CurrentState.CurrentFile is None:
+                            ic("CurrentFile not set")
+                            time.sleep(1)
+                            continue
+
+                        readable, _, _ = select.select([log_fd], [], [], 0.5)
+                        # print(f"readable = {readable}")
+                        if not readable:
+                            if file_check():
+                                continue
+                            else:
+                                break
+                        line = log_fd.readline()
+                        if line is None or line == "":
+                            if file_check():
+                                continue
+                            else:
+                                # print(f"Restarting {str(log_file)}")
+                                break
+                        location = log_fd.tell()
+                        # print(f"{line}, now at {location}")
+                        self.process_line(line)
+                    # I need to make it so that it tries reading the new file
 
             except FileNotFoundError:
                 # This is expected
                 time.sleep(1)
 
     def process_line(self, line: str) -> None:
+        ic(f"Processing line {line}")
         results = self.chunk_search_re.search(line.strip())
         if results is not None:
             chunk_hex = results.group(1)
@@ -134,6 +188,7 @@ class BzPrepare:
                 "timestamp": str(datetime.now()).split(".")[0],
                 "publish_count": self.publish_count,
             }
+            ic(f"Emitting chunk {chunk_number}")
             self.backup_status.signals.get_messages.emit(message)
 
         return
--- src/backblaze_status/bz_transmit.py
+++ src/backblaze_status/bz_transmit.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 from io import TextIOWrapper
 from pathlib import Path
 
-from .backup_file import BackupFile
+from .constants import Key, MessageTypes
 from .qt_backup_status import QTBackupStatus
 from .to_do_files import ToDoFiles
 from .utils import MultiLogger
@@ -15,8 +15,9 @@ from .utils import MultiLogger
 @dataclass
 class BzTransmit:
     """
-    The bztransmit log contains a lot of information about the backup process. I ignore most of it, but there are
-    certain key lines that give insights into what is happening in the backup
+    The bztransmit log contains a lot of information about the backup process.
+    I ignore most of it, but there are certain key lines that give insights into what
+    is happening in the backup
     """
 
     backup_status: QTBackupStatus
@@ -28,7 +29,7 @@ class BzTransmit:
     _blank_lines: int = field(default=0, init=False)
     _bytes: int = field(default=0, init=False)
     _batch_count: int = field(default=0, init=False)
-    _is_batch: bool = field(default=False, init=False)
+    publish_count: int = field(default=0, init=False)
     _current_filename: Path | None = field(default=None, init=False)
     _first_pass: bool = field(default=True, init=False)
     BZ_LOG_DIR: str = field(
@@ -57,10 +58,13 @@ class BzTransmit:
 
         # Compile duplicate chunk
         self.duplicate_encrypted = re.compile(
-            "noCopy code path VERY SUCCESSFUL,.*largeFileName=([^,]*).*_seq([^\.]*)"
+            "noCopy code path VERY SUCCESSFUL,.*largeFileName=([^,]*).*_seq([^.]*)"
+            # "noCopy code path VERY SUCCESSFUL,.*largeFileName=([^,]*).*_seq([^\.]*)"
         )
 
-    def _get_latest_logfile_name(self) -> Path:
+        # self.backblaze_redis = BackBlazeRedis()
+
+    def get_latest_logfile_name(self) -> Path:
         """
         Scan the log directory for any files that end in .log, and return the one
         with the newest modification time
@@ -78,7 +82,7 @@ class BzTransmit:
                         last_file = _file
         return last_file
 
-    def _process_line(self, _line: str) -> None:
+    def process_line(self, _line: str) -> None:
         _filename: Path | None = None
         _bytes: int = 0
         _rate: str | None = None
@@ -106,22 +110,23 @@ class BzTransmit:
                     .replace(tzinfo=timezone.utc)
                     .astimezone(tz=None)
                 )
-            chunk = True
             _filename = Path(_line.split(": ")[-1].rstrip())
+            filename = str(_filename)
 
-            if not self.to_do.exists(str(_filename)):
-                self.to_do.add_file(
-                    str(_filename),
-                    is_chunk=chunk,
-                )
-            self.to_do.current_file = self.to_do.get_file(str(_filename))
-            self.backup_status.signals.start_new_file.emit(str(_filename))
+            self.emit_message(
+                MessageTypes.StartNewFile,
+                f"New file is {filename}",
+                {
+                    Key.FileName: filename,
+                    Key.StartTime: _datetime,
+                },
+            )
 
         _dedup_search_results = self.dedup_search_re.search(_line)
         if _dedup_search_results is not None:
-            chunk = True
             chunk_number = int(_dedup_search_results.group(1))
             _filename = Path(_line.split(": ")[-1].rstrip())
+            filename = str(_filename)
             _timestamp = _line[0:19]
             if _timestamp[4] == "-":
                 # The transmit file uses UTC time
@@ -138,53 +143,11 @@ class BzTransmit:
                     .astimezone(tz=None)
                 )
 
-            if not self.to_do.exists(str(_filename)):
-                self.to_do.add_file(
-                    str(_filename),
-                    is_chunk=chunk,
-                )  # add_file locks the backup list itself
-
-            backup_file = self.to_do.get_file(str(_filename))  # type: BackupFile
-            backup_file.add_deduped(chunk_number)
-            self.backup_status.chunk_model.layoutChanged.emit()
-            # ic(f"chunk layoutChanged in bztransmit for dedup")
-            backup_file.current_chunk = chunk_number
-            backup_file.rate = "bztransmit"
-
-        # _dedup_encrypted_search_results = self.duplicate_encrypted.search(_line)
-        # if _dedup_encrypted_search_results is not None:
-        #     chunk = True
-        #     chunk_number = int(_dedup_encrypted_search_results.group(2), base=16)
-        #     _filename = Path(_dedup_encrypted_search_results.group(1))
-        #     _timestamp = _line[0:19]
-        #     if _timestamp[4] == "-":
-        #         # The transmit file uses UTC time
-        #         _datetime = (
-        #             datetime.strptime(_timestamp, "%Y-%m-%d %H:%M:%S")
-        #             .replace(tzinfo=timezone.utc)
-        #             .astimezone(tz=None)
-        #         )
-        #     else:
-        #         _timestamp = _line[0:14]
-        #         _datetime = (
-        #             datetime.strptime(_timestamp, "%Y%m%d%H%M%S")
-        #             .replace(tzinfo=timezone.utc)
-        #             .astimezone(tz=None)
-        #         )
-        #
-        #     if not self.to_do.exists(str(_filename)):
-        #         self.to_do.add_file(
-        #             Path(_filename),
-        #             is_chunk=chunk,
-        #             timestamp=_datetime,
-        #         )  # add_file locks the backup list itself
-        #
-        #     backup_file = self.to_do.get_file(str(_filename))  # type: BackupFile
-        #     backup_file.add_deduped(chunk_number)
-        #     self.qt.chunk_model.layoutChanged.emit()
-        #     # ic(f"chunk layoutChanged in bztransmit for transmit")
-        #     backup_file.current_chunk = chunk_number
-        #     backup_file.rate = "bztransmit"
+            self.emit_message(
+                MessageTypes.AddDedupedChunk,
+                f"Add deduped chunk {chunk_number} to {filename}",
+                {Key.FileName: filename, Key.Chunk: chunk_number},
+            )
 
         _new_to_do_file_search_results = self.new_to_do_file_re.search(_line)
         if _new_to_do_file_search_results is not None:
@@ -192,8 +155,19 @@ class BzTransmit:
             self._multi_log.log(f"There is a new ToDo file. Reread it ({_timestamp})")
             pass
 
+    def emit_message(self, message_type: str, message_string: str, data: dict) -> None:
+        self.publish_count += 1
+        message = {
+            "type": message_type,
+            "message": message_string,
+            "data": data,
+            "timestamp": str(datetime.now()).split(".")[0],
+            "publish_count": self.publish_count,
+        }
+        self.backup_status.signals.get_messages.emit(message)
+
     def read_file(self) -> None:
-        _log_file = self._get_latest_logfile_name()
+        _log_file = self.get_latest_logfile_name()
         self._current_filename = _log_file
         while True:
             with _log_file.open("r") as _log_fd:
@@ -201,10 +175,10 @@ class BzTransmit:
                 if self._first_pass:
                     _log_fd.seek(0, 2)
                 for _line in self._tail_file(_log_fd):
-                    self._process_line(_line)
+                    self.process_line(_line)
 
                 self._first_pass = False
-                _log_file = self._get_latest_logfile_name()
+                _log_file = self.get_latest_logfile_name()
                 self._current_filename = _log_file
 
     def _tail_file(self, _file: TextIOWrapper) -> str:
@@ -215,8 +189,8 @@ class BzTransmit:
                 if self._first_pass:
                     self._multi_log.log("Finished first pass", module=self._module_name)
                 self._first_pass = False
-                time.sleep(1)
-                _new_filename = self._get_latest_logfile_name()
+                time.sleep(0.5)
+                _new_filename = self.get_latest_logfile_name()
                 if _new_filename != self._current_filename:
                     return
                 continue
