6a836e083283bf14a21c0bf29519a4a407e5b713
Xev <38051173+xevg@users.noreply.github.com>
2024-05-08 19:24:47 -0400

    refactor(Signals): add comments and clean up

--- src/backblaze_status/signals.py
+++ src/backblaze_status/signals.py
@@ -5,39 +5,22 @@ from PyQt6.QtCore import (
 
 
 class Signals(QObject):
-    # GUI Regular Updated
+    """
+    A class that contains all the signals needed for the application
+    """
 
-    # update_clock = pyqtSignal(str)
-    # update_progress_bar = pyqtSignal(dict)
     update_stats_box = pyqtSignal()
     go_to_current_row = pyqtSignal()
     update_chunk_progress = pyqtSignal()
-
-    # set_current_file = pyqtSignal(str)
-
-    # calculate_progress = pyqtSignal()
-
-    # Manage interval timer
-
-    # interval_timer = pyqtSignal(int)
-    # stop_interval_timer = pyqtSignal()
 
     # Data triggers
 
     to_do_available = pyqtSignal()
     start_new_file = pyqtSignal(str)
-    # files_updated = pyqtSignal()
 
-    # To Do Triggers
+    # To Do read messages
 
-    # add_file = pyqtSignal(str, bool)
-    # mark_completed = pyqtSignal(str)
     get_messages = pyqtSignal(dict)
-
-    # Modes
-
-    # preparing = pyqtSignal()
-    # transmitting = pyqtSignal(str)
 
     # Backup Running
     backup_running = pyqtSignal(bool)
