778183c355a8f064841d4569e27e5f3c33193ac3
Xev <38051173+xevg@users.noreply.github.com>
2024-05-08 12:32:33 -0400

    refactor: comments

--- src/backblaze_status/__init__.py
+++ src/backblaze_status/__init__.py
@@ -12,10 +12,12 @@ from .qt_backup_status import QTBackupStatus
 __version__ = version("backblaze_status")
 __extra_version__ = "v0.10.1"
 
+# For better error display
 rich.traceback.install(show_locals=False)
 
 
 def run():
+    # Start the application and set the icon and application name
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon("backblaze_status.png"))
     app.setApplicationName("Backblaze Status")
